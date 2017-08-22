8d418f37d34ed056e2e921f2d65d9033c37b2ad0
nico203 <nico.203@hotmail.com>
2017-08-22 13:07:38 -0300

    refactor

--- src/filters/highlight.filter.js
+++
@@ -1,9 +0,0 @@
-angular.module('adminPanel').filter('highlight', ['$sce', function ($sce) {
-    return function (text, phrase) {
-        if (phrase) {
-            text = text.replace(new RegExp('(' + phrase + ')', 'gi'),
-                    '<span class="highlighted">$1</span>');
-        }
-        return $sce.trustAsHtml(text);
-    };
-}]);
