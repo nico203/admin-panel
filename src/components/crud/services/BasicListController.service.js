834c6aeec5de20c56eeee9c54fd4877729a1c731
nico203 <nico.203@hotmail.com>
2017-10-25 12:55:15 -0300

    cambios

---
+++ src/components/crud/services/BasicListController.service.js
@@ -0,0 +1,11 @@
+/* 
+ * Servicio para listar todos los elementos 
+ */
+
+angular.module('adminPanel.crud').service('BasicListController', [
+    'CrudConfig',
+    function(CrudConfig) {
+        
+    }
+]);
+
