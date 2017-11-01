cad34b8fdcfc93f7a81034ed7b7a66f54eb81dfc
nico203 <nico.203@hotmail.com>
2017-11-01 13:00:35 -0300

    parcial listcontroller

--- src/components/crud/services/BasicListController.service.js
+++ src/components/crud/services/BasicListController.service.js
@@ -1,42 +1,60 @@
 /* 
  * Servicio para listar todos los elementos 
+ * 
+ * FALTA implementar los resultados en base a un hijo
+ * 
+ * FALTA implementar busqueda
  */
-
 angular.module('adminPanel.crud').service('BasicListController', [
     'CrudConfig','$timeout',
     function(CrudConfig,$timeout) {
         
         /**
-         * @description Inicializa el controlador del componente para listar entidades del servidor
+         * @description Lista los objetos de una entidad. Si la respuesta desde el servidor es de la forma 
+         * object: {
+         *    totalItemCount: 'numero total de entidades en el servidor',
+         *    pageNumber: 'Numero de la pagina actual'
+         * }
+         * implementa paginacion sobre los elementos devueltos.
          * 
-         * @param {Controller} controller Controller del componente
-         * @param {CrudResource} resource Recurso del servidor a usar para obtener los datos
          * @param {Scope} scope Scope del componente
+         * @param {CrudResource} resource Recurso del servidor a usar para obtener los datos
          * @param {String} apLoadName | Nombre de la directiva load al que apuntar para ocultar la vista en los intercambios con el servidor
-         * @returns {undefined}
          */
         function BasicListController(scope, resource, apLoadName) {
             scope.list = [];
             var request = null;
             
             /**
-             * Inicializa el controlador
+             * @description Inicializa el controlador
              * 
              * @returns {BasicListController}
              */
             this.init = function () {
                 this.list();
-                
                 return this;
             };
             
+            /**
+             * @description Lista los elementos de la entidad en la base de datos.
+             * 
+             * @param {Object} params parametros adicionales para hacer en el request.
+             * @param {string} actionDefault accion a interpretar del servidor. Por defecto, 'get'.
+             * @param {function} callbackSuccess funcion que es llamada al traer los datos del servidor, luego de que
+             * se asignen los datos a la lista de la entidad y se cancele el evento de carga en la vista. 
+             * Recibe como parametro la respuesta del servidor
+             * @param {function} callbackError funcion que es llamada en caso de haber un error, luego de que se cancele 
+             * el evento de carga en la vista. Recibe como parametro la respuesta del servidor.
+             * 
+             * @returns {BasicListController}
+             */
             this.list = function(params, actionDefault, callbackSuccess, callbackError) {
                 var listParams = (params) ? params : {};
                 
                 $timeout(function () {
                     //se muestra el gif de carga
                     scope.$broadcast('apLoad:start',apLoadName);
-                    var action = (actionDefault) ? actionDefault : 'get';
+                    var action = (typeof(actionDefault) === 'string') ? actionDefault : 'get';
                     
                     //si hay un request en proceso se lo cancela
                     if(request && !request.$promise.$resolved) {
@@ -50,16 +68,16 @@ angular.module('adminPanel.crud').service('BasicListController', [
                         scope.$broadcast('apLoad:finish',apLoadName);
                         
                         //se listan las entidades obtenidas del request
-                        scope.list = r.data;
+                        scope.list = responseSuccess.data;
                         
                         //se envia el evento para paginar, si es que la respuesta contiene los datos para paginacion
                         scope.$broadcast('pagination:paginate', {
-                            totalPageCount: r.totalPageCount,
-                            currentPageNumber: r.currentPageNumber
+                            totalPageCount: responseSuccess.totalPageCount,
+                            currentPageNumber: responseSuccess.currentPageNumber
                         });
                         
                         //si hay un callback en caso de exito, se lo llama y se pasa como parametro la respuesta
-                        if(callbackSuccess) {
+                        if(typeof(callbackSuccess) === 'function') {
                             callbackSuccess(responseSuccess);
                         }
                     }, function(responseError) {
@@ -72,7 +90,7 @@ angular.module('adminPanel.crud').service('BasicListController', [
                         });
                         
                         //si hay un callback en caso de error, se lo llama y se pasa como parametro la respuesta
-                        if(callbackError) {
+                        if(typeof(callbackError) === 'function') {
                             callbackError(responseError);
                         }
                     });
@@ -80,12 +98,14 @@ angular.module('adminPanel.crud').service('BasicListController', [
                     //aregamos el request al scope para poderlo cancelar
                     scope.request = request;
                 });
+                
+                return  this;
             };
             
             //cancelamos los request al destruir el controller
             this.destroy = function() {
-                if(scope.request) {
-                    scope.request.$cancelRequest();
+                if(request) {
+                    request.$cancelRequest();
                 }
             };
             
@@ -97,6 +117,8 @@ angular.module('adminPanel.crud').service('BasicListController', [
                 });
             });
         }
+        
+        return BasicListController;
     }
 ]);
 
--- src/components/crud/services/BasicReadController.service.js
+++ src/components/crud/services/BasicReadController.service.js
@@ -1,7 +1,79 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Servicio para obtener los datos de una entidad en especifico desde un servidor
+ * 
+ * FALTA implementar los resultados en base a un hijo
  */
+angular.module('adminPanel.crud').service('BasicReadController', [
+    'CrudConfig','$timeout',
+    function(CrudConfig,$timeout) {
+        
+        /**
+         * @description 
+         * 
+         * @param {Scope} scope Scope del componente
+         * @param {CrudResource} resource Recurso del servidor a usar para obtener los datos
+         * @param {String} apLoadName | Nombre de la directiva load al que apuntar para ocultar la vista en los intercambios con el servidor
+         */
+        function BasicReadController(scope, resource, apLoadName) {
+            var request = null;
+            
+            this.get = function(params, actionDefault, callbackSuccess, callbackError) {
+                
 
-
+                //esta definida la propiedad, es decir tiene un sub recurso 
+                // pero este proviene de otro lugar y no hay que obtenerlo del servidor
+                if(property && !(angular.isUndefined(this[name][property]) || this[name][property] === null)) {
+                    scope[name] = this[name];
+                    if(scope[name][property]) {
+                        scope[property] = scope[name][property];
+                    } else {
+                        scope[name][property] = scope[property];
+                    }
+                    if(callbackInit) {
+                        callbackInit();
+                    }
+                    return;
+                } 
+                
+                //los datos se obtienen del servidor 
+                if(this[name] && this[name] !== 'nuevo') {
+                    var obj = {};
+                    obj[name] = this[name];
+                    form.init(obj, function(r) {
+                        scope[name] = r.data;
+                        if(property) {
+                            if(scope[name][property]) {
+                                scope[property] = scope[name][property];
+                            } else {
+                                scope[name][property] = r.data[property];
+                            }
+                        }
+                        if(callbackInit) {
+                            callbackInit();
+                        }
+                    });
+                }
+            };
+            
+            
+            /**
+             * @description Inicializa el controlador
+             * 
+             * @returns {BasicReadController}
+             */
+            this.init = function() {
+                this.get();
+                return this;
+            };
+            
+            //cancelamos los request al destruir el controller
+            this.destroy = function() {
+                if(scope.request) {
+                    scope.request.$cancelRequest();
+                }
+            };
+        }
+        
+        return BasicReadController;
+    }
+]);
