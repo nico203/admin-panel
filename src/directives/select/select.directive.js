7de03bd6717b9a282884c0164c328b752b1d4edd
nico203 <nico.203@hotmail.com>
2017-11-27 11:38:11 -0300

    agregada posibilidad de agregar un parametro para hacer la peticion al servidor en directiva select

--- src/directives/select/select.directive.js
+++ src/directives/select/select.directive.js
@@ -30,6 +30,7 @@ angular.module('adminPanel').directive('apSelect', [
                 resource: '@',
                 queryParams: '=?',
                 method: '=?',
+                requestParam: '=?',
                 properties: '='
             },
             link: function (scope, elem, attr, ngModel) {
@@ -108,6 +109,8 @@ angular.module('adminPanel').directive('apSelect', [
                  * 
                  * El parametro all establece que se haga una consulta sin parametros.
                  * Esta se hace cuando se inicializa el componente y todavia no se hizo ningun request
+                 * 
+                 * @param {boolean} all Establece si se usan los filtros para filtrar las entidades 
                  */
                 function doRequest(all) {
                     if(request) {
@@ -115,6 +118,10 @@ angular.module('adminPanel').directive('apSelect', [
                     }
                     
                     var search = {};
+                    
+                    if(!angular.isUndefined(scope.requestParam) && angular.isNumber(scope.requestParam)) {
+                        search['id'] = scope.requestParam;
+                    }
                     
                     if(!all) {
                         for (var j = 0; j < queryParams.length; j++) {
