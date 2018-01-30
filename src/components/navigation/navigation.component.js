716da64411710a5d3b1e60e2cb538857adad35f4
nico203 <nico.203@hotmail.com>
2018-01-29 21:24:37 -0300

    refactorizacionsass topbar y navigation

--- src/components/navigation/navigation.component.js
+++ src/components/navigation/navigation.component.js
@@ -1,6 +1,40 @@
-function navigationController($scope, $timeout, AdminPanelConfig) {
+function navigationController($scope, $timeout, AdminPanelConfig, $location) {
     $scope.items = AdminPanelConfig.navigationItems;
     $scope.elem = $('navigation');
+    $scope.currentRoute = null;
+    $scope.baseIndex = null;
+    
+    function changeRoute(route) {
+        $scope.currentRoute = route;
+        var index = 0;
+        for(var item in $scope.items) {
+            if($scope.items[item].link === '#') {
+                //el elemento tiene items anidados
+                for(var nestedItem in $scope.items[item].items) {
+                    var r = $scope.items[item].items[nestedItem];
+                    if(r.slice(r.indexOf('/')) === route) {
+                        $scope.baseIndex = index;
+                        break;
+                    }
+                }
+            } else {
+                //el elemento no tiene items anidados por lo tanto se checkea la ruta
+                var routeAux = $scope.items[item].link.slice($scope.items[item].link.indexOf('/'));
+                if(routeAux === route) {
+                    $scope.baseIndex = index;
+                    break;
+                }
+            }
+            index++;
+        }
+    }
+    
+    $scope.checkRoute = function(route) {
+        var routeAux = route.slice(route.indexOf('/'));
+        return {
+            'is-active': routeAux === $scope.currentRoute
+        };
+    };
     
     this.$onInit = function() {
         //En este caso $timeout es usado para ejecutar una funcion despues de que termine el ciclo $digest actual
@@ -11,6 +45,8 @@ function navigationController($scope, $timeout, AdminPanelConfig) {
             $scope.accordion = new Foundation.AccordionMenu($scope.elem);
             $scope.elem.find('> .menu').addClass('visible');
         });
+        
+        changeRoute($location.path());
     };
 
     this.$onDestroy = function() {
@@ -18,11 +54,15 @@ function navigationController($scope, $timeout, AdminPanelConfig) {
             $scope.accordion.$element.foundation('_destroy');
         }
     };
+    
+    $scope.$on('$routeChangeSuccess', function(e, route) {
+        changeRoute($location.path());
+    });
 }
 
 angular.module('adminPanel.navigation', [
     'adminPanel'
 ]).component('navigation', {
     templateUrl: 'components/navigation/navigation.template.html',
-    controller: ['$scope', '$timeout', 'AdminPanelConfig', navigationController]
+    controller: ['$scope', '$timeout', 'AdminPanelConfig', '$location', navigationController]
 });
