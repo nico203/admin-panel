92a4a52f4d7e8a837f71aa9279bc00c6846d7ee4
nico203 <nico.203@hotmail.com>
2018-02-22 17:09:01 -0300

    parcial

--- dist/js/admin-panel.js
+++ dist/js/admin-panel.js
@@ -2852,7 +2852,7 @@ angular.module('adminPanel').directive('apSelect', [
 });;angular.module('adminPanel').service('AudioService', [
     '$injector',
     function($injector) {
-        var src = 'https://dl-web.dropbox.com/get/Guardados/h.oga?_subject_uid=49679938&duc_id=dropbox_duc_id&w=AABhm3LKQcWWZXC8t4py2dnsdFTD4QXTuJpM7J5OmOHF3w';
+        var src = 'https://s0.vocaroo.com/media/download_temp/Vocaroo_s0PN2g9vvPeC.webm';
         
         this.play = function() {
             var config = $injector.has('appConfig') ? $injector.get('appConfig') : null;
