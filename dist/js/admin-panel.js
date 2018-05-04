0c24011f5aa5de5ef045989c6a4d0ab7ed6ac184
Matias Santa <matiasesanta@gmail.com>
2018-05-04 17:55:25 -0300

    mejorar componente navigation

--- dist/js/admin-panel.js
+++ dist/js/admin-panel.js
@@ -1136,101 +1136,159 @@ angular.module('adminPanel.crud').service('NormalizeService', [
             return object;
         };
     }
-]);;function navigationController($scope, $timeout, AdminPanelConfig, $location) {
-    $scope.items = AdminPanelConfig.navigationItems;
-    $scope.elem = $('navigation');
-    $scope.activeRole = null;
-    $scope.currentRoute = null;
-    $scope.baseIndex = null;
+]);;angular.module('adminPanel.navigation', [
+    'adminPanel'
+]).
 
-    $scope.showItem = function (data) {
-        if (!data.roles || !$scope.activeRole) {
-            return true;
-        }
-        if (angular.isArray(data.roles)) {
-            return data.roles.some(function (element) {
-                return isActiveRole(element);
-            });
-        }
-        return isActiveRole(data.roles);
-    };
-
-    function isActiveRole(element) {
-        if (angular.isArray($scope.activeRole)) {
-            return $scope.activeRole.includes(element);
-        }
-        return element === $scope.activeRole;
-    }
-
-    $scope.$on('userData', function(e, data) {
-        if (data) {
-            $scope.activeRole = data.roles;
-        } else {
+component('navigation', {
+    templateUrl: 'components/navigation/navigation.template.html',
+    controller: ['$scope', '$timeout', 'AdminPanelConfig', '$location',
+        function ($scope, $timeout, AdminPanelConfig, $location) {
+            $scope.items = {};
+            $scope.elem = $('navigation');
             $scope.activeRole = null;
-        }
-    });
-
-    function changeRoute(route) {
-        $scope.currentRoute = route;
-        var index = 0;
-        for(var item in $scope.items) {
-            if($scope.items[item].link === '#') {
-                //el elemento tiene items anidados
-                for(var nestedItem in $scope.items[item].items) {
-                    var r = $scope.items[item].items[nestedItem].link;
-                    if(r.slice(r.indexOf('/')) === route) {
-                        $scope.baseIndex = index;
-                        break;
-                    }
+            $scope.currentRoute = null;
+            $scope.baseIndex = null;
+        
+            /**
+             * @description Indica si un item de la lista debe mostrarse o no
+             * 
+             * @param {object} item Item de la lista
+             * @returns {boolean}
+             */
+            function showItem(item) {
+                if (!item.roles || !$scope.activeRole) {
+                    return true;
                 }
-            } else {
-                //el elemento no tiene items anidados por lo tanto se checkea la ruta
-                var routeAux = $scope.items[item].link.slice($scope.items[item].link.indexOf('/'));
-                if(routeAux === route) {
-                    $scope.baseIndex = index;
-                    break;
+                if (angular.isArray(item.roles)) {
+                    return item.roles.some(function(role) {
+                        return isActiveRole(role);
+                    });
+                }
+                return isActiveRole(item.roles);
+            }
+        
+            /**
+             * @description Indica si un determinado rol de usuario se ecnuentra activo o no
+             * 
+             * @param {(string|Array)} role Rol o roles de usuario
+             * @returns {boolean}
+             */
+            function isActiveRole(role) {
+                if (angular.isArray($scope.activeRole)) {
+                    return $scope.activeRole.includes(role);
+                }
+                return role === $scope.activeRole;
+            }
+        
+            /**
+             * @description Incializar accordion de Foundation
+             */
+            function initializeAccordion() {
+                $timeout(function() {
+                    $scope.accordion = new Foundation.AccordionMenu($scope.elem);
+                    $scope.elem.find('> .menu').addClass('visible');
+
+                    var nestedItemActive = $scope.elem.find('> .menu .nested li.is-active a');
+                    if (nestedItemActive && nestedItemActive.length > 0) {
+                        nestedItemActive = nestedItemActive.parent().parent();
+                        $scope.accordion.$element.foundation('down', nestedItemActive);
+                    }
+                });
+            }
+
+
+            /**
+             * @description Destruir accordion
+             */
+            function destroyAccordion() {
+                if($scope.accordion) {
+                    $scope.accordion.$element.foundation('_destroy');
                 }
             }
-            index++;
+
+            /**
+             * @description Genera los items del menu basándose en $scope.activeRole
+             */
+            function generateItems() {
+                $scope.items = {};
+                for(var item in AdminPanelConfig.navigationItems) {
+                    if (showItem(AdminPanelConfig.navigationItems[item])) {
+                        $scope.items[item] = angular.copy(AdminPanelConfig.navigationItems[item]);
+                    }
+                }
+            }
+        
+            /**
+             * @description Función que se llama cuando se cambia de ruta y cuando se inicializa el accordion.
+             * 
+             * @param {string} route ruta actual
+             */
+            function changeRoute(route) {
+                $scope.currentRoute = route;
+                var index = 0;
+                for(var item in $scope.items) {
+                    if($scope.items[item].link === '#') {
+                        //el elemento tiene items anidados
+                        for(var nestedItem in $scope.items[item].items) {
+                            var r = $scope.items[item].items[nestedItem].link;
+                            if(r.slice(r.indexOf('/')) === route) {
+                                $scope.baseIndex = index;
+                                break;
+                            }
+                        }
+                    } else {
+                        //el elemento no tiene items anidados por lo tanto se checkea la ruta
+                        var routeAux = $scope.items[item].link.slice($scope.items[item].link.indexOf('/'));
+                        if(routeAux === route) {
+                            $scope.baseIndex = index;
+                            break;
+                        }
+                    }
+                    index++;
+                }
+            }
+
+            /**
+             * @description Evento que recibe los datos del usuario logueado y modifica el menu según el rol.
+             * 
+             * @param {object} e Event
+             * @param {object} data Datos de usuario. Ej: data: {roles: ['ROLE1', 'ROLE2']}
+             */
+            $scope.$on('userData', function(e, data) {
+                if (data) {
+                    if ($scope.activeRole !== data.roles) {
+                        $scope.activeRole = data.roles;
+                        generateItems();
+                        destroyAccordion();
+                        initializeAccordion();
+                        changeRoute($location.path());
+                    }
+                } else {
+                    $scope.items = {};
+                    $scope.activeRole = null;
+                    destroyAccordion();
+                    initializeAccordion();
+                    changeRoute($location.path());
+                }
+            });
+        
+            $scope.checkRoute = function(route) {
+                var routeAux = route.slice(route.indexOf('/'));
+                return {
+                    'is-active': routeAux === $scope.currentRoute
+                };
+            };
+        
+            this.$onDestroy = function() {
+                destroyAccordion();
+            };
+        
+            $scope.$on('$routeChangeSuccess', function(e, route) {
+                changeRoute($location.path());
+            });
         }
-    }
-
-    $scope.checkRoute = function(route) {
-        var routeAux = route.slice(route.indexOf('/'));
-        return {
-            'is-active': routeAux === $scope.currentRoute
-        };
-    };
-
-    this.$onInit = function() {
-        //En este caso $timeout es usado para ejecutar una funcion despues de que termine el ciclo $digest actual
-        //cuando se termino de linkear todos los elementos de ngRepeat
-        //https://stackoverflow.com/questions/15207788/calling-a-function-when-ng-repeat-has-finished
-        $timeout(function() {
-            $scope.elem = $('navigation');
-            $scope.accordion = new Foundation.AccordionMenu($scope.elem);
-            $scope.elem.find('> .menu').addClass('visible');
-        });
-
-        changeRoute($location.path());
-    };
-
-    this.$onDestroy = function() {
-        if($scope.accordion) {
-            $scope.accordion.$element.foundation('_destroy');
-        }
-    };
-
-    $scope.$on('$routeChangeSuccess', function(e, route) {
-        changeRoute($location.path());
-    });
-}
-
-angular.module('adminPanel.navigation', [
-    'adminPanel'
-]).component('navigation', {
-    templateUrl: 'components/navigation/navigation.template.html',
-    controller: ['$scope', '$timeout', 'AdminPanelConfig', '$location', navigationController]
+    ]
 });;function topBarController($scope, AuthenticationService, $location) {
     $scope.clickBtn = function() {
         AuthenticationService.logout();
@@ -2933,7 +2991,7 @@ angular.module('adminPanel').directive('apSelect', [
   $templateCache.put("components/crud/directives/list/listContainer.template.html",
     "<ap-box title={{title}}><div ng-if=newRoute class=row><a ng-href={{newRoute}} class=button>Nuevo</a></div><div ng-transclude=form></div><div ap-load><div class=small-12 ng-transclude=list></div></div></ap-box>");
   $templateCache.put("components/navigation/navigation.template.html",
-    "<ul class=\"vertical menu\"><li ng-repeat=\"(name, item) in items\" ng-class=\"{'is-active': baseIndex === $index}\"><a ng-if=showItem(item) href={{item.link}} ng-bind=name></a><ul ng-if=\"showItem(item) && item.items\" class=\"vertical menu nested\"><li ng-repeat=\"(nestedItemName, nestedItemData) in item.items\" ng-class=checkRoute(nestedItemData.link)><a ng-if=showItem(nestedItemData) href={{nestedItemData.link}} ng-bind=nestedItemName></a></li></ul></li></ul>");
+    "<ul class=\"vertical menu\"><li ng-repeat=\"(name, item) in items\" ng-class=\"{'is-active': baseIndex === $index}\"><a href={{item.link}} ng-bind=name></a><ul ng-if=item.items class=\"vertical menu nested\"><li ng-repeat=\"(nestedItemName, nestedItemData) in item.items\" ng-class=checkRoute(nestedItemData.link)><a href={{nestedItemData.link}} ng-bind=nestedItemName></a></li></ul></li></ul>");
   $templateCache.put("components/top-bar/top-bar.template.html",
     "<div class=top-bar><div class=top-bar-left></div><div class=top-bar-right><ul class=menu><li><button type=button class=button ng-click=clickBtn()>Cerrar Sesión</button></li></ul></div></div>");
   $templateCache.put("directives/accordion/accordion.template.html",
