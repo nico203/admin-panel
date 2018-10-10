ed97205d272349604c5657b4b821f80c89f929ec
Matias Santa <matiasesanta@gmail.com>
2018-10-10 20:54:16 -0300

    agregar factory para inicializar controladores de filtros de tabla

--- dist/js/admin-panel.js
+++ dist/js/admin-panel.js
@@ -126,6 +126,59 @@ angular.module('adminPanel.crud').directive('apDeleteContainer',[
     }
 ]);
 ;/**
+ * Controlador para filtros de tablas.
+ */
+angular.module('adminPanel.crud').factory('BasicFilterController', [
+    '$location',
+    function($location) {
+        function BasicFilterController($scope) {
+            var self = this;
+
+            /**
+             * @description Actualiza la ruta y emite un evento con los datos del formulario
+             * 
+             * @param transform Función que transforma los datos antes de aplicar el filtro (opcional)
+             */
+            self.filter = function(transform) {
+                if (transform) {
+                    transform($scope.filtros);
+                }
+                $location.search($scope.filtros);
+                $scope.$emit('filter', angular.copy($scope.filtros));
+            };
+            
+            /**
+             * @description Limpia el formulario y emite un evento
+             */
+            self.clear = function() {
+                $scope.filtros = {
+                    exclusiveSearch: true
+                };
+                self.filter();
+            };
+            
+            /**
+             * @description Inicializa el controlador
+             * 
+             * @param transform Función que inicializa los datos del filtro (opcional)
+             * @returns {BasicFilterController}
+             */
+            self.init = function(transform) {
+                $scope.filtros = angular.isDefined($location.search()) ? $location.search() : {};
+                $scope.filtros.exclusiveSearch = true;
+                
+                if (transform) {
+                    transform($scope.filtros);
+                }
+                
+                self.filter();
+                return self;
+            };
+        }
+        
+        return BasicFilterController;
+    }
+]);;/**
  * Obtiene los datos del servidor para ser editados.
  * Los expone a través de un atributo definido en el $scope del componente según la propiedad 'name' de
  * la instancia de CrudResource que se provea.
