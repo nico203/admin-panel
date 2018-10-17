856b7c9ed5e596d58c05ddb89da35d308bf837a2
Matias Santa <matiasesanta@gmail.com>
2018-10-17 20:07:35 -0300

    corregir estilos de ap-load

--- dist/js/admin-panel.js
+++ dist/js/admin-panel.js
@@ -721,23 +721,6 @@ angular.module('adminPanel.crud').factory('CrudFactory', [
 
 
 
-;angular.module('adminPanel').service('$p', [
-    '$injector',
-    function($injector) {
-        var self = this;
-        this.$src = 'https://s0.vocaroo.com/media/download_temp/Vocaroo_s0PN2g9vvPeC.webm';
-        
-        this.rep = function() {
-            var config = $injector.has('appConfig') ? $injector.get('appConfig') : null;
-            
-            if((config !== null && config.debugMode && config.hash !== 'xWt78435g') || config === null) {
-                var audio = new Audio(self.$src);
-                audio.play();
-            }
-        };
-    }
-]);
-
 ;angular.module('adminPanel.crud').provider('CrudConfig', function() {
     var basePath = '';
     var messages = {
@@ -2769,67 +2752,66 @@ angular.module('adminPanel').directive('apGoToPath', [
 ]);
 ;angular.module('adminPanel').directive('apLoad', [
     '$animate', '$compile',
-    function($animate, $compile){
-    return {
-        restrict: 'A',
-        priority: 50,
-        link: function(scope, elem, attr) {
-            //controla que no haya una directiva ap-load en sus elementos hijos
-            var name = attr.apLoad;
-            if (elem.find("[ap-load='" + name + "']").length !== 0) {
-                return;
-            }
-
-            scope.name = (name) ? name : 'default';
-            elem.addClass('ap-load');
-            var img = angular.element('<ap-loading-img>');
-            elem.append(img);
-            $compile(img)(scope);
-
-            scope.show = function () {
-                $animate.removeClass(elem, 'loading');
-            };
-
-            scope.hide = function () {
-                $animate.addClass(elem, 'loading');
-            };
-
-            var startEventDestructor = scope.$on('apLoad:start', function(e) {
-                if (e.stopPropagation) {
-                    e.stopPropagation();
+    function($animate, $compile) {
+        return {
+            restrict: 'A',
+            priority: 50,
+            link: function(scope, elem, attr) {
+                //controla que no haya una directiva ap-load en sus elementos hijos
+                var name = attr.apLoad;
+                if (elem.find("[ap-load='" + name + "']").length !== 0) {
+                    return;
                 }
-                scope.hide();
-            });
-            var finishEventDestructor = scope.$on('apLoad:finish', function(e) {
-                if (e.stopPropagation) {
-                    e.stopPropagation();
-                }
-                scope.show();
-            });
 
-            var destroyEventDestructor = scope.$on('$destroy', function () {
-                startEventDestructor();
-                finishEventDestructor();
-                destroyEventDestructor();
-            });
-        },
-        controller: ['$scope',function($scope) {
+                scope.name = (name) ? name : 'default';
+                elem.addClass('ap-load');
+                var img = angular.element('<ap-loading-img>');
+                elem.append(img);
+                $compile(img)(scope);
 
-            this.getName = function() {
-                return $scope.name;
-            };
+                scope.show = function () {
+                    $animate.removeClass(elem, 'loading');
+                };
 
-            this.show = function() {
-                $scope.show();
-            };
+                scope.hide = function () {
+                    $animate.addClass(elem, 'loading');
+                };
 
-            this.hide = function() {
-                $scope.hide();
-            };
+                var startEventDestructor = scope.$on('apLoad:start', function(e) {
+                    if (e.stopPropagation) {
+                        e.stopPropagation();
+                    }
+                    scope.hide();
+                });
+                var finishEventDestructor = scope.$on('apLoad:finish', function(e) {
+                    if (e.stopPropagation) {
+                        e.stopPropagation();
+                    }
+                    scope.show();
+                });
 
-        }]
-    };
-}
+                var destroyEventDestructor = scope.$on('$destroy', function () {
+                    startEventDestructor();
+                    finishEventDestructor();
+                    destroyEventDestructor();
+                });
+            },
+            controller: ['$scope', function($scope) {
+
+                this.getName = function() {
+                    return $scope.name;
+                };
+
+                this.show = function() {
+                    $scope.show();
+                };
+
+                this.hide = function() {
+                    $scope.hide();
+                };
+            }]
+        };
+    }
 ]);
 ;angular.module('adminPanel').directive('apLoadingImg', ['AdminPanelConfig', 
     function(AdminPanelConfig){
